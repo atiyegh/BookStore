63c23488694b9a024fc47f717745abe89bff2d4a
Atiye ghorbani <a.ghorbani@deltacenter.local>
2022-11-03 09:40:48 +0330

    loading

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,23 +3,27 @@ import { getBookList } from '../utils/api';
 import BookCard from '../Components/BookList/BookCard/BookCard';
 import '../Style/Home.scss';
 import notify from "../Toastify/Toast";
-
+import { LoadingOutlined } from '@ant-design/icons';
 
 function Home() {
     const [bookList, setBookList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         getbookList()
     }, [])
 
     const getbookList = async () => {
+        setIsLoading(true)
         try {
             const books = await getBookList()
             //console.log([...books])
+            setIsLoading(false)
             setBookList([...books])
             //console.log(bookList)
 
         } catch (error) {
+            setIsLoading(false)
             console.log(error)
             notify(error.message, "error");
         }
@@ -28,16 +32,21 @@ function Home() {
     return (
         <div className='container'>
             {
-                bookList.map((book) => {
-                    //console.log(book)
-                    return <BookCard 
-                    coverUri={book.coverUri} 
-                    title={book.title}
-                    authours={book.authors}     
-                    price={book.price}
-                    rating={book.rating}
-                    />
-                })
+                isLoading ?
+                    <div className='loadingFrame'>
+                        <LoadingOutlined />
+                        <p>لیست کتابها در حال بارگذاری است...</p>
+                    </div> :
+                    bookList.map((book) => {
+                        //console.log(book)
+                        return <BookCard
+                            coverUri={book.coverUri}
+                            title={book.title}
+                            authours={book.authors}
+                            price={book.price}
+                            rating={book.rating}
+                        />
+                    })
             }
         </div>
     )
