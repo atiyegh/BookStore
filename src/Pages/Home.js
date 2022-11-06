caf4598f7aa9daafe910623dea1d3c9bf17393a1
Atiye ghorbani <a.ghorbani@deltacenter.local>
2022-11-06 16:43:03 +0330

    filter

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,16 +5,68 @@ import '../Style/Home.scss';
 import notify from "../Toastify/Toast";
 import { LoadingOutlined } from '@ant-design/icons';
 import { Alert } from 'antd';
-import {Error_Messages} from '../utils/Errors/ErrorMessages';
+import { Error_Messages } from '../utils/Errors/ErrorMessages';
+import { TreeSelect } from 'antd';
+
+const treeData = [
+    {
+        title: 'صوتی',
+        value: 'Voice',
+    },
+    {
+        title: 'متنی',
+        value: 'Text',
+        children: [
+            {
+                title: 'ایپاب',
+                value: 'epub',
+            },
+            {
+                title: 'پی دی اف',
+                value: 'pdf',
+            },
+        ],
+    }
+];
+
 
 function Home() {
     const [bookList, setBookList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
+    const [bookType, setBookType] = useState();
+    const [fileType,setFileType]=useState([]);
+
 
     useEffect(() => {
-        getbookList()
+        getbookList();
+        determineFileType();
     }, [])
+
+    const determineFileType=()=>{
+        let fileTypeBook=[];
+
+        const getbooksFileType=()=>bookList.map((book)=>{
+            const files=book.files.filter((file)=>file.type===3)
+            //console.log(files)
+            if(files.length>0){
+                fileTypeBook.push('epub')
+            }else{
+                fileTypeBook.push('pdf')
+            }
+        })
+
+        getbooksFileType();
+        //console.log(fileTypeBook);
+        setFileType([...fileTypeBook]);
+        //console.log(fileType);
+        
+    };
+
+    const changeBookType = (newValue) => {
+        console.log(newValue);
+        setBookType(newValue);
+    };
 
     const getbookList = async () => {
         setIsLoading(true)
@@ -35,15 +87,40 @@ function Home() {
 
     return (
         <div className='container'>
-            {
-                isLoading ?
-                    <div className='loadingFrame'>
-                        <LoadingOutlined />
-                        <p>لیست کتابها در حال بارگذاری است...</p>
-                    </div> :
-                    bookList.map((book,index) => {
-                        
-                        return (
+            <div className='Filterbox'>
+                <TreeSelect
+                    style={{
+                        width: '100%',
+                    }}
+                    value={bookType}
+                    dropdownStyle={{
+                        maxHeight: 400,
+                        overflow: 'auto',
+                    }}
+                    treeData={treeData}
+                    placeholder="نوع کتاب را انتخاب نمایید"
+                    treeDefaultExpandAll
+                    onChange={changeBookType}
+                />
+            </div>
+            <div className='listBox'>
+                {
+                    isLoading ?
+                        <div className='loadingFrame'>
+                            <LoadingOutlined />
+                            <p>لیست کتابها در حال بارگذاری است...</p>
+                        </div> :
+                        bookList.filter((book,index)=>{
+                            if(book.type==='Text'){
+                                console.log(fileType[index]===bookType)
+                                return fileType[index]===bookType
+                            }else {
+                                console.log(book.type==='Voice')
+                                return book.type==='Voice'
+                            }
+                        }).map((book) => {
+                            //console.log(book)
+                            return (
                                 <BookCard
                                     coverUri={book.coverUri}
                                     title={book.title}
@@ -56,10 +133,12 @@ function Home() {
                                     description={book.description}
                                     id={book.id}
                                 />
-                        )
+                            )
 
-                    })
-            }
+                        })
+                }
+            </div>
+
             {
                 isError && <Alert
                     message="خطای اتصال به شبکه"
